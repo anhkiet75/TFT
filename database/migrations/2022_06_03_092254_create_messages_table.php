da81bde6af119af454fad952bf710eafb924aeee
Kiet Tien Anh <takiet1@tma.com.vn>
2022-06-03 17:53:26 +0700

    first commit

---
+++ database/migrations/2022_06_03_092254_create_messages_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->string('message');
+            $table->string('message_type');
+            $table->foreignId('conversation_id');
+            $table->foreignId('sender_id');
+            $table->string('attachment_thumb_url');
+            $table->string('attachment_url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+};
